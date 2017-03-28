c8ae8c096c6a66dd1e1add56ee99bb9f4eb3d811
Julien TRÉBOSC <julien.trebosc@univ-lille1f.r>
2017-03-28 15:19:05 +0200

    created a viewfid command to replace the viewfid.jt AU program

---
+++ TSpy/viewfid.py
@@ -0,0 +1,26 @@
+# set all optionnal processing parameters to 0
+import sys
+import os
+print sys.path.append(os.getenv("PYTHONPATH"))
+from brukerPAR import dataset
+
+ProcOptions = ["WDW", "PH_mod", "BC_mod", "ME_mod", "FT_mod"]
+dat = dataset(CURDATA())
+
+Stored=[{},{}]
+for dim in [0, 1]:
+    for par in ProcOptions:
+        Stored[dim][par]=GETPAR("%d %s" % (dim+1,par))
+#        MSG("%d %s" % (dim+1,par)+" is "+Stored[dim][par])
+for dim in [0, 1]:
+    for par in ProcOptions:
+        dat.writeprocpar(par, "0", dimension=2-dim, status=False)
+        # PUTPAR("%d %s" % (dim+1,par),"0")
+print Stored
+
+XTRF()
+
+for dim in [0, 1]:
+    for par in ProcOptions:
+        dat.writeprocpar(par, Stored[dim][par], dimension=2-dim, status=False)
+
