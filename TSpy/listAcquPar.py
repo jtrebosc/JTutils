8019179eeb4cd08dc964336273314bb8152c90da
Julien TRÉBOSC <julien.trebosc@univ-lille1f.r>
2018-02-15 18:47:56 +0100

    better formating

--- TSpy/listAcquPar.py
+++ TSpy/listAcquPar.py
@@ -15,8 +15,8 @@ options:
 import os
 import sys
 
-DIRINST=os.path.dirname(sys.argv[0])+"/../"
-PYTHONPATH=os.getenv("PYTHONPATH")
+DIRINST = os.path.dirname(sys.argv[0]) + "/../"
+PYTHONPATH = os.getenv("PYTHONPATH")
 #sys.path.append(PYTHONPATH)
 sys.path.append(DIRINST+"CpyLib")
 
@@ -24,46 +24,50 @@ import brukerPAR
 
 
 try: 
-	args=sys.argv[1:]
-	test=args[0]
+    args = sys.argv[1:]
+    test = args[0]
 except : 
-	MSG(help)
-	EXIT()
+    MSG(help)
+    EXIT()
 
-param=args[0]
-param=param.replace('.',' ')
-if '2' in args[1:] : D=2
-else : D=1
+param = args[0]
+param = param.replace('.', ' ')
+if '2' in args[1:] : 
+    D = 2
+else :
+    D = 1
 
-if 's' in args[1:] : S=True
-else : S=False
+if 's' in args[1:] : 
+    S = True
+else : 
+    S = False
 
 MSG("looking in dimension %d and file status %s" % (D,S))
 def searchAcqus(initdir):
-        """ search the acqus file in sub-directory of initdir"""
-        import os, fnmatch
-        pattern='acqus'
-        liste=[]
-        for path, dirs, files in os.walk(os.path.abspath(initdir)):
-                for filename in fnmatch.filter(files, pattern):
-                        liste.append(path)
-        return liste
+    """ search the acqus file in sub-directory of initdir"""
+    import os, fnmatch
+    pattern = 'acqus'
+    liste = []
+    for path, dirs, files in os.walk(os.path.abspath(initdir)):
+        for filename in fnmatch.filter(files, pattern):
+            liste.append(path)
+    return liste
 
-dataset=CURDATA()
+dataset = CURDATA()
 
-dt=brukerPAR.dataset(dataset)
+dt = brukerPAR.dataset(dataset)
 
-expnamedir=dt.returnacqpath()+'/../'
+expnamedir = dt.returnacqpath() + '/../'
 
-expnoList=searchAcqus(expnamedir)
-res=[]
+expnoList = searchAcqus(expnamedir)
+res = []
 for i in expnoList :
-	dataset=brukerPAR.splitprocpath(i+'/pdata/1/')
-	dt=brukerPAR.dataset(dataset)
-	expno=dt.dataset[1]
-	val=dt.readacqpar(param,status=S,dimension=D)
-	res.append("%s\t%s" % (str(expno),str(val)))
+    dataset = brukerPAR.splitprocpath(i + '/pdata/1/')
+    dt = brukerPAR.dataset(dataset)
+    expno = dt.dataset[1]
+    val = dt.readacqpar(param, status=S, dimension=D)
+    res.append("%s\t%s" % (str(expno), str(val)))
 res.sort(key=lambda s : int(s.split()[0]))
-print "looking for '"+" ".join(args)+"' in "+expnamedir
-print '\n'.join(res)
+print "looking for '" + " ".join(args) + "' in " + expnamedir
+print('\n'.join(res))
 MSG('\n'.join(res))
