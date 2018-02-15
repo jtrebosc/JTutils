20f253a025f13c19c118e2b41395d7d5ea19eb80
Julien TRÉBOSC <julien.trebosc@univ-lille1f.r>
2018-02-15 18:49:46 +0100

    added some sort of argument checking

--- TSpy/mkax.py
+++ TSpy/mkax.py
@@ -5,19 +5,36 @@ import os.path
 
 # installation directory is relative to current script location
 # DIRINST=os.path.dirname(sys.argv[0])+"/../"
+def usage():
+	MSG("usage mkax first_step step_incr n_step [ax_unit]")
 
 DST=CURDATA()
 if len(sys.argv)<4:
-	MSG("usage mkax first_step step_incr n_step [ax_unit]")
+    usage()
 	EXIT()
 if len(sys.argv)==5:
 	axunit=sys.argv[4]
 else:
 	axunit=None
 
-p1=float(sys.argv[1])
-p2=float(sys.argv[2])
-n=float(sys.argv[3])
+try:
+    p1=float(sys.argv[1])
+except ValueError:
+    MSG("First argument %s must be convertible to a float" % (sys.argv[1] ,))
+    usage()
+    EXIT()
+try:
+    p2=float(sys.argv[2])
+except ValueError:
+    MSG("second argument %s must be convertible to a float" % (sys.argv[2],))
+    usage()
+    EXIT()
+try:
+    n=int(sys.argv[3])
+except ValueError:
+    MSG("Third argument %s must be convertible to an int" % (sys.argv[2],))
+    usage()
+    EXIT()
 axleft=p1-0.5*p2
 axright=p1+(n-0.5)*p2
 
