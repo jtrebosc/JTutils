4af9f2a98d9125f5b41382fa1a16f38007c6f734
Julien TRÉBOSC <julien.trebosc@univ-lille1f.r>
2018-06-08 23:26:48 +0200

    cosmetics : ../TSpy/reverseUdeft.py : nicer code, mostly spaces

--- TSpy/reverseUdeft.py
+++ TSpy/reverseUdeft.py
@@ -5,7 +5,7 @@ import os
 import os.path
 import subprocess
 
-'''
+
 import argparse
 descriptProg = """
 Reverse UDEFT FID for easy processing of refocused echo. 
@@ -17,38 +17,39 @@ Only TDEFF points are stored back corresponding only to refocussed echo.
 """
 
 parser = argparse.ArgumentParser(description=descriptProg)
-parser.add_argument('--tdeff', required=True, type=int,
-                    help='only keep tdeff points')
-parser.add_argument('infile', help='Full path of the dataset to process')
-'''
+parser.add_argument('--td', required=False, type=int, default=0,
+                    help='actual FID td points')
+args = parser.parse_args()
 
 # installation directory is relative to current script location
-DIRINST=os.path.dirname(sys.argv[0])+"/../"
+DIRINST = os.path.dirname(sys.argv[0]) + "/../"
 # where is the external python executable
-CPYTHON=os.getenv('CPYTHON',"NotDefined")
+CPYTHON = os.getenv('CPYTHON', "NotDefined")
 if "python" not in CPYTHON:
 		MSG("CPYTHON environment not defined")
 		EXIT()
 # MSG(CPYTHON)
 
 
-dataset=CURDATA()
-TDEFF=GETPAR("TDeff")
-TD=GETPAR("TD")
-
+dataset = CURDATA()
+TDEFF = GETPAR("TDeff")
+if not args.td:
+    TD = GETPAR("TD")
+else:
+    TD = args.td 
 # special treatment for topspin<3
 def fullpath(dataset):
-	dat=dataset[:] # make a copy because I don't want to modify the original array
-	if len(dat)==5: # for topspin 2-
-	        dat[3]="%s/data/%s/nmr" % (dat[3],dat[4])
-	fulldata="%s/%s/%s/pdata/%s/" % (dat[3],dat[0],dat[1],dat[2])
+	dat = dataset[:]   # make a copy because I don't want to modify the original array
+	if len(dat) == 5:    # for topspin 2-
+	        dat[3] = "%s/data/%s/nmr" % (dat[3], dat[4])
+	fulldata = "%s/%s/%s/pdata/%s/" % (dat[3], dat[0], dat[1], dat[2])
 	return fulldata
-fulldataPATH=fullpath(dataset)
+fulldataPATH = fullpath(dataset)
 
-opt_args="--tdeff %s --td %s" % (TDEFF, TD)
+opt_args = "--tdeff %s --td %s" % (TDEFF, TD)
 
-script=os.path.expanduser(DIRINST+"/CpyBin/reverseUdeft_.py")
-# os.system(" ".join((CPYTHON,script,opt_args,fulldataPATH)))
-subprocess.call([CPYTHON]+[script]+opt_args.split()+[fulldataPATH])    
+script = os.path.expanduser(DIRINST + "/CpyBin/reverseUdeft_.py")
+# os.system(" ".join((CPYTHON, script, opt_args, fulldataPATH)))
+subprocess.call([CPYTHON] + [script] + opt_args.split() + [fulldataPATH])    
 
 RE(dataset)
