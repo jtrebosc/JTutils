8bef8e2bc2d8e5989a37f286b459256eafa181f5
Julien TREBOSC <julien.trebosc@univ-lille.fr>
2019-07-15 10:53:33 +0200

    add diagnostic script : base for future installation script

--- TSpy/JTinfo_sys.py
+++ TSpy/JTinfo_sys.py
@@ -39,17 +39,17 @@ try :
     import JTutils
     import_report += "JTutils module imported successfully\n"
 except ImportError:
-    import_report += "JTutils module import FAILED\n"
+    import_report += "ERROR: JTutils module import FAILED\n"
 try :
     from JTutils import brukerPAR
     import_report += "brukerPAR module imported successfully\n"
 except ImportError:
-    import_report += "brukerPAR module import FAILED\n"
+    import_report += "ERROR: brukerPAR module import FAILED\n"
 try :
     import argparse
     import_report += "argparse module imported successfully\n"
 except ImportError:
-    import_report += """argparse module import FAILED: you should consider installing argparse.py 
+    import_report += """ERROR: argparse module import FAILED: you should consider installing argparse.py 
  in %s
  or %s \n""" % (XWINNMRHOME + "/jython/Lib/", XWINNMRHOME + "/exp/stan/nmr/py/user/",)
     import_report += "argparse.py module file can be downloaded from https://pypi.org/project/argparse/ website\n"
@@ -60,53 +60,71 @@ import subprocess
 
 WarningCpython = "Checking CPYTHON configuration\n"
 
-CMD = """import sys
+if CPYTHON=="not defined" :
+    WarningCpython += """ERROR: CPYTHON variable is not defined in environnement
+     you need to define CPYTHON environment variable 
+     to a working excutable of python with numpy module\n"""
+    cpython_available = False
+else:
+    CMD = """import sys
 print(sys.version) """
-try:
-    cpython_version = subprocess.check_output([CPYTHON,"-c", CMD], stderr=subprocess.STDOUT)
-    WarningCpython += "CPYTHON version is %s\n" % (cpython_version,)
-except :
-    WarningCpython += "CPYTHON raised an error: %s" % (cpython_version,)
+    try:
+        cpython_version = subprocess.check_output([CPYTHON,"-c", CMD], stderr=subprocess.STDOUT)
+        WarningCpython += "%s version is %s\n" % (CPYTHON, cpython_version,)
+        cpython_available = True
+    except :
+        exc, exc_mesg = sys.exc_info()[0:2]
+        WarningCpython += "ERROR: %s raised an error: %s\n%s" % (CPYTHON, exc, exc_mesg)
+        cpython_available = False
 
-CMD = """import numpy
+if cpython_available:
+    CMD = """import numpy
 print(numpy.__version__) """
-try:
-    cnumpy_version = subprocess.check_output([CPYTHON,"-c", CMD], stderr=subprocess.STDOUT)
-    WarningCpython += "CPYTHON numpy version is %s\n" % (cnumpy_version,)
-except :
-    WarningCpython += "numpy module in CPYTHON raised an error: %s\n" % (cnumpy_version,)
+    try:
+        cnumpy_version = subprocess.check_output([CPYTHON,"-c", CMD], stderr=subprocess.STDOUT)
+        WarningCpython += "in %s numpy module version is %s\n" % (CPYTHON, cnumpy_version,)
+    except subprocess.CalledProcessError, exc:
+        WarningCpython += "ERROR: numpy module in %s raised an error: %s\n" % (CPYTHON, exc.output,)
 
-CMD = """import argparse
+    CMD = """import argparse
 print(argparse.__version__) """
-try:
-    cargparse_version = subprocess.check_output([CPYTHON,"-c", CMD], stderr=subprocess.STDOUT)
-    WarningCpython += "CPYTHON argparse version is %s\n" % (cargparse_version,)
-except :
-    WarningCpython += "argparse module in CPYTHON raised an error: %s\n" % (cargparse_version,)
+    try:
+        cargparse_version = subprocess.check_output([CPYTHON,"-c", CMD], stderr=subprocess.STDOUT)
+        WarningCpython += "in %s argparse module version is %s\n" % (CPYTHON, cargparse_version,)
+    except subprocess.CalledProcessError, exc:
+        WarningCpython += "argparse module in %s raised an error: %s\n" % (CPYTHON, exc.output,)
 
-CMD = """import processing
-print(processing.__version__) """
-try:
-    cprocessing_version = subprocess.check_output([CPYTHON,"-c", CMD], stderr=subprocess.STDOUT)
-    WarningCpython += "CPYTHON processing version is %s\n" % (cprocessing_version,)
-except :
-    WarningCpython += "processing module in CPYTHON raised an error: %s \n" % (subprocess.STDOUT,)
+    CMD = """import multiprocessing
+print(multiprocessing.__version__) """
+    try:
+        cprocessing_version = subprocess.check_output([CPYTHON,"-c", CMD], stderr=subprocess.STDOUT)
+        WarningCpython += "In %s multiprocessing module version is %s\n" % (CPYTHON, cprocessing_version,)
+    except subprocess.CalledProcessError, exc:
+        WarningCpython += "multiprocessing module in %s raised an error: %s \n" % (CPYTHON, exc.output,)
 
-CMD = """import bruker
+    CMD = """import bruker
 print(bruker.__version__) """
-try:
-    cbruker_version = subprocess.check_output([CPYTHON,"-c", CMD], stderr=subprocess.STDOUT)
-    WarningCpython += "CPYTHON bruker version is %s\n" % (cbruker_version,)
-except :
-    WarningCpython += "bruker module in CPYTHON raised an error: %s\n" % (cbruker_version,)
+    try:
+        cbruker_version = subprocess.check_output([CPYTHON,"-c", CMD], stderr=subprocess.STDOUT)
+        WarningCpython += "In %s bruker module version is %s\n" % (CPYTHON, cbruker_version,)
+    except subprocess.CalledProcessError, exc:
+        WarningCpython += "bruker module in %s raised an error: %s\n" % (CPYTHON, exc.output,)
 
 message += WarningLibDir
 message += import_report
 message += WarningCpython
 MSG(message)
 
-env_list = "List of defined environment variables"
+env_list = "\nList of defined environment variables\n"
 env_list += "\n".join(["%s: %s" % (key,var_env[key]) for key in var_env.keys()])
 
 print(env_list)
+
+save_report = CONFIRM(title="Save report", message="Do you want to save the report in a file ?")
+if save_report :
+    # add a file dialog to choose the directory and filename where to store report
+    f = open("report.txt",'w')
+    f.write(message)
+    f.write(env_list)
+    MSG("""report written in %s.""" % (curdir+"/report.txt",))
 
