ce1a05138fda012aec7958b1bd256be19a52c598
Julien TREBOSC <julien.trebosc@univ-lille.fr>
2019-11-26 16:14:51 +0100

    qcpmgaddWin.py: convert script to use JTutils/qcpmgadd.py script as a module

--- TSpy/qcpmgaddWin.py
+++ TSpy/qcpmgaddWin.py
@@ -1,7 +1,6 @@
 # Simple GUI example
 from javax.swing import *
 from java.awt import *
-# import optparse
 
 import sys
 
@@ -11,70 +10,61 @@ import os
 import os.path
 import subprocess
 
-"""
-import argparse
-
-parser = argparse.ArgumentParser(description='Add echoes in a qcpmg bruker experiment')
-parser.add_argument('-l','--lb',type=float, help='Lorentzian broadening applied to the decaying echo',default=0)
-parser.add_argument('-g','--gb',type=float, help='Gaussian broadening applied to each echo',default=0)
-parser.add_argument('-n',type=int, help='Number of echo to sum')
-parser.add_argument('-c',type=float, help='qcpmg cycle in us')
-parser.add_argument('infile',help='Full path of the dataset to process')
-"""
+from JTutils.TSpy import qcpmgadd
 
 #installation directory is relative to current script location
-DIRINST=os.path.dirname(sys.argv[0])+"/../"
+DIRINST = os.path.dirname(sys.argv[0])+"/../"
 # where is the external python executable
-CPYTHON=os.getenv('CPYTHON',"NotDefined")
+CPYTHON = os.getenv('CPYTHON', "NotDefined")
 if "python" not in CPYTHON:
 		MSG("CPYTHON environment not defined")
 		EXIT()
 #MSG(CPYTHON)
 
 
-dataset=CURDATA()
-N=str(1+int(GETPARSTAT("L 22")))
-LB=GETPAR("LB")
-GB=GETPAR("USERP1")
-cycle=float(GETPARSTAT("P 60"))
+dataset = CURDATA()
+N = str(1+int(GETPARSTAT("L 22")))
+LB = GETPAR("LB")
+GB = GETPAR("USERP1")
+cycle = float(GETPARSTAT("P 60"))
 if cycle < 1: # P60 is not likely to have stored the cycle time then uses historic calculation
     # historic qcpmg.jt cycle calculation
-    D3=float(GETPARSTAT("D 3"))*1e6
-    D6=float(GETPARSTAT("D 6"))*1e6
-    P4=float(GETPARSTAT("P 4"))
-    cycle=2*(D3+D6)+P4
-cycle=str(cycle)
-PUTPAR("LB",LB)
-PUTPAR("USERP1",GB)
+    D3 = float(GETPARSTAT("D 3"))*1e6
+    D6 = float(GETPARSTAT("D 6"))*1e6
+    P4 = float(GETPARSTAT("P 4"))
+    cycle = 2*(D3+D6)+P4
+cycle = str(cycle)
+PUTPAR("LB", LB)
+PUTPAR("USERP1", GB)
 
 
 print cycle
 # special treatment for topspin<3
 def fullpath(dataset):
-    dat=dataset[:] # make a copy because I don't want to modify the original array
-    if len(dat)==5: # for topspin 2-
-        dat[3]="%s/data/%s/nmr" % (dat[3],dat[4])
-    fulldata="%s/%s/%s/pdata/%s/" % (dat[3],dat[0],dat[1],dat[2])
+    dat = dataset[:] # make a copy because I don't want to modify the original array
+    if len(dat) == 5: # for topspin 2-
+        dat[3] = "%s/data/%s/nmr" % (dat[3], dat[4])
+    fulldata = "%s/%s/%s/pdata/%s/" % (dat[3], dat[0], dat[1], dat[2])
     return fulldata
 
 def canceled(event):
 	frame0.dispose()
 
 def validated(event):
-    (GB,LB,N,cycle)= [JTFgb.getText(),JTFlb.getText(),
-                      JTFslope.getText(),JTFcycle.getText()]
-    opt_args=" -g %s -l %s -n %s -c %s" % (GB,LB,N,cycle)
-    if echoB.isSelected():
-        opt_args+=" -o "
-    if aechoB.isSelected():
-        opt_args+=" -e "
-    script=os.path.expanduser(DIRINST+"/CpyBin/qcpmgadd_.py")
-    #	os.system(" ".join((CPYTHON,script,opt_args,fulldataPATH)))
+    (GB, LB, N, cycle) = [JTF_gb.getText(), JTF_lb.getText(),
+                      JTF_slope.getText(), JTF_cycle.getText()]
+    opt_args = " -g %s -l %s -n %s -c %s" % (GB, LB, N, cycle)
+    if JRB_oddecho.isSelected():
+        opt_args += " -o "
+    if JRB_evenecho.isSelected():
+        opt_args += " -e "
+    script = os.path.expanduser(DIRINST+"/CpyBin/qcpmgadd_.py")
+    #	os.system(" ".join((CPYTHON, script, opt_args, fulldataPATH)))
     subprocess.call([CPYTHON]+[script]+opt_args.split()+[fulldataPATH])
     frame0.dispose()
     EXEC_PYSCRIPT("RE_PATH('%s')"%(fulldataPATH,))
 
-fulldataPATH=fullpath(dataset)
+fulldataPATH = fullpath(dataset)
 
 """
 JLabel("GB:")  
@@ -89,54 +79,54 @@ button_HELP
 """
 
 # defined a frame with 2 buttons
-Lgb=JLabel("GB", SwingConstants.RIGHT)
-JTFgb=JTextField(GB)
-Llb=JLabel("LB", SwingConstants.RIGHT)
-JTFlb=JTextField(LB)
-Lslope=JLabel("N", SwingConstants.RIGHT)
-JTFslope=JTextField(N)
-Lcycle=JLabel("Cycle", SwingConstants.RIGHT)
-JTFcycle=JTextField(cycle)
+JL_gb = JLabel("GB", SwingConstants.RIGHT)
+JTF_gb = JTextField(GB)
+JL_lb = JLabel("LB", SwingConstants.RIGHT)
+JTF_lb = JTextField(LB)
+JL_slope = JLabel("N", SwingConstants.RIGHT)
+JTF_slope = JTextField(N)
+JL_cycle = JLabel("Cycle", SwingConstants.RIGHT)
+JTF_cycle = JTextField(cycle)
 
-echoB=JRadioButton("sum odd echoes")
-aechoB=JRadioButton("sum even echoes")
-bechoB=JRadioButton("sum all echoes")
+JRB_oddecho = JRadioButton("sum odd echoes")
+JRB_evenecho = JRadioButton("sum even echoes")
+JRB_bothecho = JRadioButton("sum all echoes")
 
-button1 = JButton('OK', actionPerformed = validated) 
-button2 = JButton('Cancel', actionPerformed = canceled)
+button1 = JButton('OK', actionPerformed=validated) 
+button2 = JButton('Cancel', actionPerformed=canceled)
 
 # create window with title
 frame0 = JFrame('TopSPin / Python GUI Example') 
 # set window size x, y
 frame0.setSize(500, 300) 
-frame0.setLayout(GridLayout(0,1)) 
+frame0.setLayout(GridLayout(0, 1)) 
 
-frame1=JPanel(GridLayout(0,2))
+frame1 = JPanel(GridLayout(0, 2))
 frame0.add(frame1)
-frame2=JPanel()
+frame2 = JPanel()
 frame0.add(frame2)
-frame3=JPanel()
+frame3 = JPanel()
 frame0.add(frame3)
 
-grpB=ButtonGroup()
-grpB.add(echoB)
-grpB.add(aechoB)
-grpB.add(bechoB)
+grpB = ButtonGroup()
+grpB.add(JRB_oddecho)
+grpB.add(JRB_evenecho)
+grpB.add(JRB_bothecho)
 
 
 
 # layout manager for horizontal alignment
-frame1.add(Lgb)
-frame1.add(JTFgb)
-frame1.add(Llb)
-frame1.add(JTFlb)
-frame1.add(Lslope)
-frame1.add(JTFslope)
-frame1.add(Lcycle)
-frame1.add(JTFcycle)
-frame2.add(echoB)
-frame2.add(aechoB)
-frame2.add(bechoB)
+frame1.add(JL_gb)
+frame1.add(JTF_gb)
+frame1.add(JL_lb)
+frame1.add(JTF_lb)
+frame1.add(JL_slope)
+frame1.add(JTF_slope)
+frame1.add(JL_cycle)
+frame1.add(JTF_cycle)
+frame2.add(JRB_oddecho)
+frame2.add(JRB_evenecho)
+frame2.add(JRB_bothecho)
 frame3.add(button1)
 frame3.add(button2)
 frame0.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE)
