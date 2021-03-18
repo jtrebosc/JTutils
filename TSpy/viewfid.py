5976a7b3de14275b4b31d702128c0bff3221c8e0
Julien TREBOSC <julien.trebosc@univ-lille.fr>
2021-03-18 11:48:40 +0100

    Clean/Comment viewfid.py script

--- TSpy/viewfid.py
+++ TSpy/viewfid.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
-# set all optionnal processing parameters to 0
+# puts a 2D ser file into the processed spectrum tab without any processing
+# If 
 import sys
 import os
 
 from JTutils.CpyLib.brukerPARIO import dataset
 current_expno = CURDATA()
+
+# Check if acquisition type is traditionnal sampling or Non-uniform sampling.
+# other kind of sampling are not handled (full(points) and projection_spectroscpy)
 
 FnTYPE = GETPAR("2s FnTYPE")
 if FnTYPE == '0':
@@ -20,7 +24,7 @@ if FnTYPE == '0':
         for par in ProcOptions:
             dat.writeprocpar(par, "0", dimension=2-dim, status=False)
             # PUTPAR("%d %s" % (dim+1,par),"0")
-    print Stored
+#    print Stored
 
     XTRF()
 
@@ -28,9 +32,10 @@ if FnTYPE == '0':
         for par in ProcOptions:
             dat.writeprocpar(par, Stored[dim][par], dimension=2-dim, status=False)
 
-if FnTYPE == '2':
+elif FnTYPE == '2':
     import JTutils
     fulldataPATH = JTutils.fullpath(current_expno)
     JTutils.run_CpyBin_script('viewfid_.py', [fulldataPATH])
-
+else: 
+    MSG("Only Traditionnal(planes) and Non-uniform_sampling FnTYPE are handled by this script.")
 RE(current_expno)
