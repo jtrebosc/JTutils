eec1725b2f0f3ab2177cb022b43c2225629a6679
Julien TREBOSC <julien.trebosc@univ-lille.fr>
2021-04-07 22:57:15 +0200

    Improve viewfid with options to account for tdeff, si or remove digital filter

--- CpyBin/viewfid_.py
+++ CpyBin/viewfid_.py
@@ -6,10 +6,10 @@
 from __future__ import print_function, division
 
 Descript="""
-OK so what is this program doing exactly ?
-Not much : it reads 2D SER file of NUS acquired dataset. 
-Order the row according to nuslist
-plot the result 
+this script reads 2D SER file of NUS acquired dataset. 
+Order the row according to nuslist, 
+Write to 2rr, 2ir or 2ii files depending on FnMODE
+There is a function to plot the result with matplotlib for testing
 """
 
 import numpy
@@ -23,20 +23,37 @@ def showser(fid2D):
     ax.set_ylabel("F1/points")
     ax.set_xlim(0,fid2D.shape[-1])
     ax.set_ylim(0,fid2D.shape[0])
-    ax.contour(fid2D, levels=[i*fid2D.max() for i in numpy.arange(0.03, 1.0, 0.1)])
-
+    ax.contour(fid2D, levels=[i*fid2D.max() 
+                               for i in numpy.arange(0.03, 1.0, 0.1)])
     p.show()
 
 # manage arguments
 import argparse
 parser = argparse.ArgumentParser(description='Read Ser file order row according to nuslist. Plot the t1/t2 dataset')
+parser.add_argument('-t', '--tdeff', help='Truncate FID according to TDEFF parameter', action='store_true')
+parser.add_argument('-s', '--si', help='Truncate or zerofill FID according to SI parameter', action='store_true')
+parser.add_argument('-d', '--rmdigfilt', help='Removes the digital filter points', action='store_true')
 parser.add_argument('infile', help='Full path of the dataset to process')
 
 args = parser.parse_args()
 dat = brukerIO.dataset(brukerIO.splitprocpath(args.infile))
 
 # read ser file 
-serfile = dat.readserc(rmGRPDLY=False)
+serfile = dat.readserc(rmGRPDLY=args.rmdigfilt)
+
+td2 = dat.readacqpar("TD", dimension=1, status=True) // 2 # ser is complex number in t2
+print(args.tdeff)
+if args.tdeff == True: # truncate according to TDeff in direct dim only
+    tdeff2 = dat.readprocpar("TDeff", dimension=1, status=False) // 2
+    if tdeff2>0 and tdeff2<td2:
+        td2 = tdeff2
+
+if args.rmdigfilt:
+    digfilt = dat.getdigfilt()
+    td2 -= digfilt
+
+serfile=serfile[:, 0:td2]
+
 # determine the hypercomplex status
 FnMode = dat.readacqpar("FnMODE", status=True, dimension=2)
 if FnMode == 0:  # if undefined then read MC2 processing parameter 
@@ -45,22 +62,24 @@ if FnMode == 0:  # if undefined then read MC2 processing parameter
     FnMode == dat.readprocpar("MC2", status=False, dimension=2) + 1
 
 td1 = dat.readacqpar("TD", dimension=2, status=True)
+
 if FnMode in [4, 5, 6]:  # State, States-TPPI, Echo-AntiEcho
     HCsize = 2
     td1 = 2*(td1//2)  # one only keeps an even number of rows
     serfile=serfile[0:td1, :]
+    names = ['2rr', '2ir']
 elif FnMode in [0, 1, 2, 3]:  # undefined, QF, QSEQ, TPPI
     HCsize = 1
+    names = ['2rr', '2ii']
+    serfile=serfile[0:td1, :]
 else:
     raise ValueError("FnMODE is outside acceptable range (0..6)!!! Problem with acqu2s or proc2 file ?")
-
 
 #check FnTYPE {0: 'traditionnal', 1: 'full(points)', 
 #              2:'non_uniform_sampling', 3: 'projection-spectroscopy'} 
 FnTYPE = dat.readacqpar("FnTYPE", status=True, dimension=1)
 if FnTYPE == 2: # NUS
     sershape = serfile.shape
-    FullF1TD = dat.readacqpar('NusTD', status=True, dimension=2)
     serfile = serfile.reshape(sershape[0]//HCsize, HCsize, sershape[-1])
     import os.path
     nuslist = []
@@ -68,9 +87,15 @@ if FnTYPE == 2: # NUS
         for line in f:
             nuslist.append(int(line.strip()))
     nuslist = nuslist[0:td1//HCsize]
-    print(nuslist)
-    print(sorted(nuslist))
+#    print(nuslist)
+#    print(sorted(nuslist))
+    
+# nusTD is original grid max TD/2
+#    FullF1TD = dat.readacqpar('NusTD', status=True, dimension=2)
+# calculates the max TD grid from max index in recorded nuslist points
+    FullF1TD = (max(nuslist)+1)*HCsize
     newser = numpy.zeros((FullF1TD//HCsize, HCsize, sershape[-1]), dtype=serfile.dtype)
+#    print(nuslist, serfile.shape)
     newser[nuslist] = serfile
 #    for i,j in enumerate(nuslist):
 #        newser[j] = serfile[i]
@@ -78,23 +103,47 @@ if FnTYPE == 2: # NUS
 else:
     newser = serfile
 
-shape = brukerIO.SInext(newser.shape)
+# truncate according to TDEFF if required
+td1 = newser.shape[0]
+# apply the TDeff in F1
+if args.tdeff == True:
+    tdeff1 = dat.readprocpar("TDeff", dimension=2, status=False)
+    if tdeff1 > 0 and tdeff1 < td1:
+        td1 = tdeff1
+        newser = newser[0:td1, :]
+
+if args.si == True:
+    SIs = (dat.readprocpar("SI", status=False, dimension=2),
+              dat.readprocpar("SI", status=False, dimension=1))
+    print("setting SI in F1")
+    print("SI1 = ", SIs[0])
+    print("td1 = ", td1)
+else:
+    SIs = newser.shape
+shape = brukerIO.SInext(SIs)
 newser = brukerIO.zeroFill(newser,shape)
-dat.writespectnd(newser.imag, name='2ir')
-dat.writespectnd(newser.real)
+dat.writespectnd(newser.imag, name=names[1])
+dat.writespectnd(newser.real, name=names[0])
 # set time units
-rank = len(shape)
+rank = len(newser.shape)
+# write some status parameters not already written by writespectnd
 for dim in range(1, rank+1):
     dat.writeprocpar("AXUNIT", "s", status=True, dimension=dim)
     dat.writeprocpar("AXLEFT", 0, status=True, dimension=dim)
     swh = dat.readacqpar("SW_h", status=True, dimension=dim)
-    si = shape[-dim]
+    si = newser.shape[-dim]
     sfo1 = dat.readacqpar("SFO1", status=True, dimension=dim)
     sf = dat.readprocpar("SF", status=False, dimension=dim)
     dat.writeprocpar("AXRIGHT", (1/swh*(si)/dim), status=True, dimension=dim)
     dat.writeprocpar("OFFSET", ((sfo1+swh/2-sf)/sf), status=True, dimension=dim)
     dat.writeprocpar("SW_p", (swh/sfo1), status=True, dimension=dim)
-    dat.writeprocpar("PKNL", "no", status=True, dimension=dim)
+    dat.writeprocpar("PH_mod", 0, status=True, dimension=dim)
+    dat.writeprocpar("BC_mod", 0, status=True, dimension=dim)
     dat.writeprocpar("Mdd_mod", 0, status=True, dimension=dim)
+
+# deal with digital filter for phase correction
+dat.writeprocpar("PKNL", args.rmdigfilt, status=True, dimension=1)
+# for further processing PKNL should be set to no is digital Filter is removed
+dat.writeprocpar("PKNL", not args.rmdigfilt, status=False, dimension=1)
 
 #showser(newser.real)
--- TSpy/viewfid.py
+++ TSpy/viewfid.py
@@ -4,38 +4,87 @@
 import sys
 import os
 
-from JTutils.CpyLib.brukerPARIO import dataset
-current_expno = CURDATA()
 
-# Check if acquisition type is traditionnal sampling or Non-uniform sampling.
-# other kind of sampling are not handled (full(points) and projection_spectroscpy)
+def viewfid(tdeff=False, si=False, rmdigfilt=False, dataset=None):
+    from JTutils.CpyLib.brukerPARIO import dataset as bio_dataset
+    # it could be interesting to add options hence switch to argparse structure
+    # tdeff : keep signal only up to TDEFF
+    # si : zero fill data up to SI parameter
+    # first process tdeff
+    # tdeff : option -t --tdeff
+    # if TDEFF=0 or TDEFF>size: no effect, size=size
+    # else size=tdeff
+    #second process the si option (impact zerofilling/truncation) 
+    # si : option -s --si : 
+    # if si<size (or tdeff if size=tdeff) : size=si, truncation occurs
+    # if si>size : zerofilling occurs
 
-FnTYPE = GETPAR("2s FnTYPE")
-if FnTYPE == '0':
-    ProcOptions = ["WDW", "PH_mod", "BC_mod", "ME_mod", "FT_mod"]
-    dat = dataset(current_expno)
+    # Check if acquisition type is traditionnal sampling or Non-uniform sampling.
+    # other kind of sampling are not handled (full(points) and projection_spectroscpy)
+    if dataset == None:
+        dataset = CURDATA()
+        RE(dataset)
 
-    Stored=[{},{}]
-    for dim in [0, 1]:
-        for par in ProcOptions:
-            Stored[dim][par]=GETPAR("%d %s" % (dim+1,par))
-    #        MSG("%d %s" % (dim+1,par)+" is "+Stored[dim][par])
-    for dim in [0, 1]:
-        for par in ProcOptions:
-            dat.writeprocpar(par, "0", dimension=2-dim, status=False)
-            # PUTPAR("%d %s" % (dim+1,par),"0")
-#    print Stored
+    FnTYPE = GETPAR("2s FnTYPE")
+    if FnTYPE == '0':
+        ProcOptions = ["WDW", "PH_mod", "BC_mod", "ME_mod", "FT_mod"]
+        dat = bio_dataset(dataset)
 
-    XTRF()
+        Stored=[{},{}]
+        for dim in [0, 1]:
+            for par in ProcOptions:
+                Stored[dim][par]=GETPAR("%d %s" % (dim+1,par))
+        #        MSG("%d %s" % (dim+1,par)+" is "+Stored[dim][par])
+        for dim in [0, 1]:
+            for par in ProcOptions:
+                dat.writeprocpar(par, "0", dimension=2-dim, status=False)
+                # PUTPAR("%d %s" % (dim+1,par),"0")
+    #    print Stored
 
-    for dim in [0, 1]:
-        for par in ProcOptions:
-            dat.writeprocpar(par, Stored[dim][par], dimension=2-dim, status=False)
+        XTRF()
 
-elif FnTYPE == '2':
-    import JTutils
-    fulldataPATH = JTutils.fullpath(current_expno)
-    JTutils.run_CpyBin_script('viewfid_.py', [fulldataPATH])
-else: 
-    MSG("Only Traditionnal(planes) and Non-uniform_sampling FnTYPE are handled by this script.")
-RE(current_expno)
+        for dim in [0, 1]:
+            for par in ProcOptions:
+                dat.writeprocpar(par, Stored[dim][par], dimension=2-dim, status=False)
+
+    elif FnTYPE == '2':
+        import JTutils
+        optns = [JTutils.fullpath(dataset)]
+        if tdeff:
+            optns.insert(0,'-t')
+        if si:
+            optns.insert(0, '-s')
+        if rmdigfilt:
+            optns.insert(0, '-d')
+        JTutils.run_CpyBin_script('viewfid_.py', optns)
+    else: 
+        MSG("Only Traditionnal(planes) and Non-uniform_sampling FnTYPE are handled by this script.")
+
+if __name__ == '__main__':
+    class dummy():
+        def __init__(self):
+            self.si = False
+            self.tdeff = False
+            self.rmdigfilt = False
+    try : 
+        import argparse
+        parser  =  argparse.ArgumentParser(description='Transfers a 2D ser file into spectrum tab for time domain observation.')
+        parser.add_argument('-t', '--tdeff', help='truncates FID to TDEFF', action='store_true')
+        parser.add_argument('-s', '--si', help='Zerofills FID to SI', action='store_true')
+        parser.add_argument('-d', '--rmdigfilt', help='Removes digital filter', action='store_true')
+        
+        args  =  parser.parse_args(sys.argv[1:])
+    except ImportError :
+        if len(sys.argv) > 1:
+            MSG("Argparse module not found!\n Arguments won't be processed")
+        args = dummy()
+    except SystemExit:
+        MSG(""" Script is exiting : either you asked for help or there is an argument error.
+        Check console for additional information
+        """  + parser.format_help() )
+        EXIT()
+
+    dtset = CURDATA()
+    viewfid(tdeff=args.tdeff, si=args.si, rmdigfilt=args.rmdigfilt, dataset=dtset)
+    RE(dtset)
+
