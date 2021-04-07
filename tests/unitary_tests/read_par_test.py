0df18140a62031a2ca46d4ad96542eb9dff2158f
Julien TREBOSC <julien.trebosc@univ-lille.fr>
2021-04-07 22:59:02 +0200

    slightly more tests

--- tests/unitary_tests/read_par_test.py
+++ tests/unitary_tests/read_par_test.py
@@ -9,7 +9,7 @@ copyfile(backup, fresh)
 
 dat = brukerIO.dataset(brukerIO.splitprocpath(sample))
 
-P = ["P 1", 
+PA = ["P 1", 
 "L 31", 
 "SPNAM 8",
 "BF1", 
@@ -17,11 +17,37 @@ P = ["P 1",
 "AUNM", 
 "LOCKED", 
 "L 64",
+"DTYPA",
 "TOTO",
 ]
-for par_name in P :
+PP = ["USERP1", 
+"TOTO",
+]
+print("reading acquisition non status parameter")
+for par_name in PA :
     try:
         par = dat.readacqpar(par_name, status=False, dimension=1)
+        print(par_name, type(par), par)
+    except Exception as e:
+        print(e)
+print("reading acquisition status parameter")
+for par_name in PA :
+    try:
+        par = dat.readacqpar(par_name, status=True, dimension=1)
+        print(par_name, type(par), par)
+    except Exception as e:
+        print(e)
+print("reading processing non status parameter")
+for par_name in PP :
+    try:
+        par = dat.readprocpar(par_name, status=False, dimension=1)
+        print(par_name, type(par), par)
+    except Exception as e:
+        print(e)
+print("reading processing status parameter")
+for par_name in PP :
+    try:
+        par = dat.readprocpar(par_name, status=True, dimension=1)
         print(par_name, type(par), par)
     except Exception as e:
         print(e)
