ea5ad97a555e07b3f97a52f8b5ebbd5e76ac578a
Julien TREBOSC <julien.trebosc@univ-lille.fr>
2021-05-28 11:49:15 +0200

    viewfid : change behavior. Use TDEFF and SI by default.

--- TSpy/viewfid.py
+++ TSpy/viewfid.py
@@ -5,22 +5,21 @@ import sys
 import os
 
 
-def viewfid(tdeff=False, si=False, rmdigfilt=False, dataset=None):
+def viewfid(notdeff=False, nosi=False, rmdigfilt=False, dataset=None):
     from JTutils.CpyLib.brukerPARIO import dataset as bio_dataset
-    # it could be interesting to add options hence switch to argparse structure
-    # tdeff : keep signal only up to TDEFF
-    # si : zero fill data up to SI parameter
-    # first process tdeff
-    # tdeff : option -t --tdeff
-    # if TDEFF=0 or TDEFF>size: no effect, size=size
-    # else size=tdeff
-    #second process the si option (impact zerofilling/truncation) 
-    # si : option -s --si : 
-    # if si<size (or tdeff if size=tdeff) : size=si, truncation occurs
-    # if si>size : zerofilling occurs
-
-    # Check if acquisition type is traditionnal sampling or Non-uniform sampling.
-    # other kind of sampling are not handled (full(points) and projection_spectroscpy)
+    """Transfers a 2D ser file in t1/t2 time domain in spectrum tab
+    by default keep signal only up to TDEFF, zero fill data up to SI parameter
+    first process tdeff
+    if TDEFF=0 or TDEFF>size: no effect, size=size
+    else size=tdeff
+    second process the si option (impact zerofilling/truncation) 
+    if si<size (or tdeff if size=tdeff) : size=si, truncation occurs
+    if si>size : zerofilling occurs
+    notdeff: if True disable TDEFF handling
+    nosi: if True : disable SI zerofill : size is set by fids size or tdeff
+    Check if acquisition type is traditionnal sampling or Non-uniform sampling.
+    other kind of sampling are not handled (full(points) and projection_spectroscpy)
+    """
     if dataset == None:
         dataset = CURDATA()
         RE(dataset)
@@ -50,9 +49,10 @@ def viewfid(tdeff=False, si=False, rmdigfilt=False, dataset=None):
     elif FnTYPE == '2':
         import JTutils
         optns = [JTutils.fullpath(dataset)]
-        if tdeff:
+        # python script options are reversed (does not use SI or TDEFF by default unless -t or -s is set)
+        if notdeff is False:  
             optns.insert(0,'-t')
-        if si:
+        if nosi is False:
             optns.insert(0, '-s')
         if rmdigfilt:
             optns.insert(0, '-d')
@@ -63,14 +63,18 @@ def viewfid(tdeff=False, si=False, rmdigfilt=False, dataset=None):
 if __name__ == '__main__':
     class dummy():
         def __init__(self):
-            self.si = False
-            self.tdeff = False
+            self.nosi = False
+            self.notdeff = False
             self.rmdigfilt = False
     try : 
         import argparse
-        parser  =  argparse.ArgumentParser(description='Transfers a 2D ser file into spectrum tab for time domain observation.')
-        parser.add_argument('-t', '--tdeff', help='truncates FID to TDEFF', action='store_true')
-        parser.add_argument('-s', '--si', help='Zerofills FID to SI', action='store_true')
+        parser  =  argparse.ArgumentParser(description="""
+        Transfers a 2D ser file into spectrum tab for time domain observation.
+        By default truncates and zerofills fids according to TDEFF and SI unless --notdeff or --nosi options is set
+        Note that is --nosi si set then minimum length to accommodate the fids is set 
+        but further processing may be impossibe""")
+        parser.add_argument('-t', '--notdeff', help='do not truncates FID to TDEFF', action='store_true')
+        parser.add_argument('-s', '--nosi', help='do not zerofills FID to SI', action='store_true')
         parser.add_argument('-d', '--rmdigfilt', help='Removes digital filter', action='store_true')
         
         args  =  parser.parse_args(sys.argv[1:])
@@ -85,6 +89,6 @@ if __name__ == '__main__':
         EXIT()
 
     dtset = CURDATA()
-    viewfid(tdeff=args.tdeff, si=args.si, rmdigfilt=args.rmdigfilt, dataset=dtset)
+    viewfid(notdeff=args.notdeff, nosi=args.nosi, rmdigfilt=args.rmdigfilt, dataset=dtset)
     RE(dtset)
 
